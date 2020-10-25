f415c63fe3331981bca7169be263ee8e11180e46
adamczakl <aadamczak@edu.cdv.pl>
2020-10-25 21:23:55 +0100

    1st comm

--- Test/index.js
+++ Test/index.js
@@ -2,6 +2,12 @@ console.log('Hello woorld');
 const express = require('express')
 const app = express()
 const port = 3000
+const express = require("express");
+const helmet = require("helmet");
+ 
+const app = express();
+ 
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,4 +15,10 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+app.get('/soap', function (req, res) {
+  res.send('Number 1!')
+})
+app.get('/rest', function (req, res) {
+  res.send('Number 2!')
 })
